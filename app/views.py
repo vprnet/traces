8996acfcbc161636a6ecdab9bf46a22a77982ce0
Matt Parrilla <matthew.parrilla@gmail.com>
2014-04-30 13:25:49 -0400

    Add share, photo, and text pages

--- app/views.py
+++ app/views.py
@@ -21,3 +21,69 @@ def index():
         page_title=page_title,
         social=social,
         page_url=page_url)
+
+
+@app.route('/share')
+def share():
+    page_url = BASE_URL + request.path
+    navbar_h1 = True
+    page_title = 'TRACES: Share Your Story'
+
+    social = {
+        'title': "",
+        'subtitle': "",
+        'img': "",
+        'description': "",
+        'twitter_text': "",
+        'twitter_hashtag': ""
+    }
+
+    return render_template('share.html',
+        page_title=page_title,
+        social=social,
+        navbar_h1=navbar_h1,
+        page_url=page_url)
+
+
+@app.route('/photo')
+def photo():
+    page_url = BASE_URL + request.path
+    navbar_h1 = True
+    page_title = 'TRACES: What Reminds You?'
+
+    social = {
+        'title': "",
+        'subtitle': "",
+        'img': "",
+        'description': "",
+        'twitter_text': "",
+        'twitter_hashtag': ""
+    }
+
+    return render_template('photo.html',
+        page_title=page_title,
+        social=social,
+        navbar_h1=navbar_h1,
+        page_url=page_url)
+
+
+@app.route('/text')
+def text():
+    page_url = BASE_URL + request.path
+    navbar_h1 = True
+    page_title = 'TRACES: What Reminds You?'
+
+    social = {
+        'title': "",
+        'subtitle': "",
+        'img': "",
+        'description': "",
+        'twitter_text': "",
+        'twitter_hashtag': ""
+    }
+
+    return render_template('text.html',
+        page_title=page_title,
+        social=social,
+        navbar_h1=navbar_h1,
+        page_url=page_url)
